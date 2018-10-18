93ffd850928be8ebca644fc10d9692a80d8e5ee1
Marek Marciński <marcinski.mar@gmail.com>
2018-10-18 19:10:07 +0200

    app initialization with startup dependencies and basic settings in application.properties file

---
+++ src/test/java/org/marcinski/chickenHouse/ChickenHouseApplicationTests.java
@@ -0,0 +1,16 @@
+package org.marcinski.chickenHouse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChickenHouseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
