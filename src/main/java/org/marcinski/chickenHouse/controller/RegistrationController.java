6b8201a7c817404c77f9a70e7cb3ad878522c24a
Marek Marciński <marcinski.mar@gmail.com>
2018-10-24 20:47:54 +0200

    registration controller

---
+++ src/main/java/org/marcinski/chickenHouse/controller/RegistrationController.java
@@ -0,0 +1,50 @@
+package org.marcinski.chickenHouse.controller;
+
+import org.marcinski.chickenHouse.model.User;
+import org.marcinski.chickenHouse.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@Controller
+public class RegistrationController {
+
+    private UserService userService;
+
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/registration")
+    public ModelAndView registration(){
+        ModelAndView modelAndView = new ModelAndView();
+        User user = new User();
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("registration");
+        return modelAndView;
+    }
+
+    @PostMapping("/registration")
+    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult){
+        ModelAndView modelAndView = new ModelAndView();
+        Optional<User> userExists = userService.findUserByEmail(user.getEmail());
+        if (!userExists.equals(Optional.empty())){
+            bindingResult.rejectValue("email", "error.user",
+                    "Istnieje już użytkownik o podanym emailu!");
+        }
+        if (bindingResult.hasErrors()){
+            modelAndView.setViewName("registration");
+        }else {
+            userService.saveUser(user);
+            modelAndView.addObject("successMessage", "Użytkownik został zarejestrowany");
+            modelAndView.addObject("user", new User());
+            modelAndView.setViewName("registration");
+        }
+        return modelAndView;
+    }
+}
