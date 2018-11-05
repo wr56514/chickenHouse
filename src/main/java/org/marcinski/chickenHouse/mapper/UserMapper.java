82070d2e0147d9535e2a1c1bd511860ce196908a
Marek Marciński <marcinski.mar@gmail.com>
2018-11-05 19:25:04 +0100

    mapper class for User

---
+++ src/main/java/org/marcinski/chickenHouse/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package org.marcinski.chickenHouse.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import org.marcinski.chickenHouse.dto.UserDto;
+import org.marcinski.chickenHouse.entity.User;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserDto mapUserEntityToUserDto(User user);
+    User mapUserDtoToUserEntity(UserDto userDto);
+}
