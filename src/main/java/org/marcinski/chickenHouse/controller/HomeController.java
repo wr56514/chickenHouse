ef1b278cf9b70c4a054d814e72760cdaf6b7b887
Marek Marciński <marcinski.mar@gmail.com>
2018-11-07 21:38:51 +0100

    added functionality of adding cookie with userId
    list of ChickenHouseDto has been added to modelAndView

--- src/main/java/org/marcinski/chickenHouse/controller/HomeController.java
+++ src/main/java/org/marcinski/chickenHouse/controller/HomeController.java
@@ -1,6 +1,8 @@
 package org.marcinski.chickenHouse.controller;
 
+import org.marcinski.chickenHouse.dto.ChickenHouseDto;
 import org.marcinski.chickenHouse.dto.UserDto;
+import org.marcinski.chickenHouse.service.ChickenHouseService;
 import org.marcinski.chickenHouse.service.UserService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -8,19 +10,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
 public class HomeController {
 
     private UserService userService;
+    private ChickenHouseService chickenHouseService;
 
-    public HomeController(UserService userService) {
+    public HomeController(UserService userService, ChickenHouseService chickenHouseService) {
         this.userService = userService;
+        this.chickenHouseService = chickenHouseService;
     }
 
     @GetMapping("/home")
-    public ModelAndView home(){
+    public ModelAndView home(HttpServletResponse response, HttpServletRequest request){
         ModelAndView modelAndView = new ModelAndView();
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         Optional<UserDto> userByEmail = userService.findUserByEmail(auth.getName());
@@ -29,10 +37,33 @@ public class HomeController {
             user = userByEmail.get();
         }
         if (user != null) {
-            modelAndView.addObject("userName", "Witamy " + user.getName() + " (" + user.getEmail() + ")");
+            modelAndView.addObject("greeting", "Cześć " + user.getName());
+            Cookie userCookie = getCookieWithUserId(user);
+            response.addCookie(userCookie);
         }
-        modelAndView.addObject("userMessage","Zawartość widoczna tylko dla zalogowanych");
+        Cookie[] cookies = request.getCookies();
+        Long userId = getUserId(cookies);
+
+        List<ChickenHouseDto> chickenHouseDto = chickenHouseService.getChickenHouseDtoByUserId(userId);
+
+        modelAndView.addObject("houses", chickenHouseDto);
         modelAndView.setViewName("home");
         return modelAndView;
+    }
+
+    private Cookie getCookieWithUserId(UserDto user) {
+        String name = "userId";
+        String value = String.valueOf(user.getId());
+        return new Cookie(name, value);
+    }
+
+    private Long getUserId(Cookie[] cookies) {
+        Long userId = null;
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("userId")){
+                userId = Long.valueOf(cookie.getValue());
+            }
+        }
+        return userId;
     }
 }
