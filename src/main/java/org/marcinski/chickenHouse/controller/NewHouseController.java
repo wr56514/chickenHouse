d081a6e9344748426b6b18172749afd07c69a44f
Marek Marciński <marcinski.mar@gmail.com>
2018-11-07 22:38:43 +0100

    controller to new_house

---
+++ src/main/java/org/marcinski/chickenHouse/controller/NewHouseController.java
@@ -0,0 +1,66 @@
+package org.marcinski.chickenHouse.controller;
+
+import org.marcinski.chickenHouse.dto.ChickenHouseDto;
+import org.marcinski.chickenHouse.dto.UserDto;
+import org.marcinski.chickenHouse.service.ChickenHouseService;
+import org.marcinski.chickenHouse.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.Optional;
+
+@Controller
+public class NewHouseController {
+
+    private UserService userService;
+    private ChickenHouseService chickenHouseService;
+
+    public NewHouseController(UserService userService, ChickenHouseService chickenHouseService) {
+        this.userService = userService;
+        this.chickenHouseService = chickenHouseService;
+    }
+
+    @GetMapping("/new_house")
+    public ModelAndView newHouse(){
+        ModelAndView modelAndView = new ModelAndView();
+        ChickenHouseDto chickenHouseDto = new ChickenHouseDto();
+        modelAndView.addObject("chickenHouseDto", chickenHouseDto);
+        modelAndView.setViewName("new_house");
+        return modelAndView;
+    }
+
+    @PostMapping("/new_house")
+    public ModelAndView addNewHouse(@Valid ChickenHouseDto chickenHouseDto,
+                                    BindingResult bindingResult,
+                                    HttpServletRequest request){
+        ModelAndView modelAndView = new ModelAndView();
+
+        Cookie[] cookies = request.getCookies();
+        Long userId = null;
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("userId")){
+                userId = Long.valueOf(cookie.getValue());
+            }
+        }
+
+        Optional<UserDto> userById = userService.findUserById(userId);
+        if (bindingResult.hasErrors()){
+            modelAndView.setViewName("new_house");
+        }
+        else {
+            if (userById.isPresent()) {
+                chickenHouseDto.setUserDto(userById.get());
+                chickenHouseService.saveChickenHouse(chickenHouseDto);
+                modelAndView.addObject("chickenHouseDto", new ChickenHouseDto());
+            }
+        }
+
+        return new ModelAndView("redirect:/home");
+    }
+}
