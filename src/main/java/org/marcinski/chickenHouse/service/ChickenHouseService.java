0da9d611695bd4cbd49e681fa6e1af36d13a75c2
Marek Marciński <marcinski.mar@gmail.com>
2018-11-07 21:34:48 +0100

    Service for ChickenHouse class, with one method - what return ChickenHouseDto List (inner mapping from ChickenHouse)

---
+++ src/main/java/org/marcinski/chickenHouse/service/ChickenHouseService.java
@@ -0,0 +1,27 @@
+package org.marcinski.chickenHouse.service;
+
+import org.marcinski.chickenHouse.dto.ChickenHouseDto;
+import org.marcinski.chickenHouse.entity.ChickenHouse;
+import org.marcinski.chickenHouse.mapper.ChickenHouseMapper;
+import org.marcinski.chickenHouse.repository.ChickenHouseRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ChickenHouseService {
+
+    private ChickenHouseRepository chickenHouseRepository;
+
+    public ChickenHouseService(ChickenHouseRepository chickenHouseRepository) {
+        this.chickenHouseRepository = chickenHouseRepository;
+    }
+
+    public List<ChickenHouseDto> getChickenHouseDtoByUserId(Long userId) {
+        List<ChickenHouse> byUserId = chickenHouseRepository.findAllByUserId(userId);
+        return byUserId.stream()
+                .map(ChickenHouseMapper.INSTANCE::mapToChickenHouseDto)
+                .collect(Collectors.toList());
+    }
+}
