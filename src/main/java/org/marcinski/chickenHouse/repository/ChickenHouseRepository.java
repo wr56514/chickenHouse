f13bea3b41f7348ae678a28d881fb5185608a83c
Marek Marciński <marcinski.mar@gmail.com>
2018-11-07 21:32:42 +0100

    repository for ChickenRepository class

---
+++ src/main/java/org/marcinski/chickenHouse/repository/ChickenHouseRepository.java
@@ -0,0 +1,10 @@
+package org.marcinski.chickenHouse.repository;
+
+import org.marcinski.chickenHouse.entity.ChickenHouse;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ChickenHouseRepository extends JpaRepository<ChickenHouse, Long> {
+    List<ChickenHouse> findAllByUserId(Long id);
+}
